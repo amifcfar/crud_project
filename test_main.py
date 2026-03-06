f0204066c08b64ac62fffc91c09151be19ffce8d
assad <amifcfar@outlook.com>
2026-03-05 21:25:13 -0500

    added test for getting item by id

--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
+from urllib import response
+
 from fastapi.testclient import TestClient
 
 from main import app, inv_test
@@ -10,15 +12,28 @@ def test_root():
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
 
-# def test_say_hello():
-#     response = client.get("/say_hello/cedric")
-#     #assert response.status_code == 200
-#     assert response.json() == { "message": "Hello cedric" }
 
-def test_get_item():
+def test_get_items():
     response = client.get("/items/")
     #assert response.status_code == 200
     assert response.json() == {"items": inv_test}
+
+
+def test_get_item_by_id():
+    response = client.get("/items/1")
+   # assert response.status_code == 200
+    assert response.json() == {"title": "puma", "description": "tout terrain", "price": 1000, "quantity": 50, "item_id": 1}
+
+
+def test_get_item_by_id2():
+    response = client.get("/items/2")
+    #assert response.status_code == 200
+    assert response.json() == {"title": "nike", "description": "puissant", "price": 2000, "quantity": 20, "item_id": 2}
+
+
+
+
+
 
 
 
